dc5af8e86587062bf7f82b843908e541885256b6
Spandan Chatterjee <spandan.chatterjee@gmail.com>
2026-06-02 15:00:14 -0700

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,10 @@
+def add_two_numbers() -> int:
+    return sum([int(number) for number in input("").split(",")])
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
